3299a89323a853a4be95f1adbd499ebca71aa1f5
Benjamin Chang <benjaminchang23@gmail.com>
2021-01-29 21:52:33 -0500

    feat: add options to database driver/tester

--- src/blacklibrarydb.cc
+++ src/blacklibrarydb.cc
@@ -4,29 +4,88 @@
 
 #include <getopt.h>
 #include <iostream>
+#include <string.h>
 
 #include <BlackLibraryDB.hh>
 
+struct options
+{
+    std::string db_path = "";
+    bool intialize_db = false;
+};
+
+static void Usage(const char *prog)
+{
+    const char *p = strchr(prog, '/');
+    printf("usage: %s --(p)ath --[i]nit_db [-h]\n", p ? (p + 1) : prog);
+}
+
+static int ParseOptions(int argc, char **argv, struct options *opts)
+{
+    static const char *const optstr = "hip:";
+    static const struct option long_opts[] = {
+        { "help", no_argument, 0, 'h' },
+        { "init_db", no_argument, 0, 'i' },
+        { "path", required_argument, 0, 'p' },
+        { "verbose", no_argument, 0, 'v' },
+        { 0, 0, 0, 0 }
+    };
+
+    if (!argv || !opts)
+        return -1;
+
+    int opt = 0;
+    while ((opt = getopt_long(argc, argv, optstr, long_opts, 0)) >= 0)
+    {
+        switch (opt)
+        {
+            case 'h':
+                Usage(argv[0]);
+                exit(0);
+                break;
+            case 'i':
+                opts->intialize_db = true;
+                break;
+            case 'p':
+                opts->db_path = std::string(optarg);
+                break;
+            default:
+                exit(1);
+                break;
+        }
+    }
+
+    if (optind < argc)
+    {
+        fprintf(stderr, "trailing options..\n");
+        exit(1);
+    }
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    // check to see if there are any extra arguments
-    if (argc > 1)
+    struct options opts;
+
+    if (argc < 2)
     {
-        std::cout << "extra arguments ";
-        for (int i = 0; i < argc; ++i)
-        {
-            std::cout << std::string(argv[i]);
-        }
-        std::cout << std::endl;
+        Usage(argv[0]);
+        exit(1);
+    }
+
+    if (ParseOptions(argc, argv, &opts))
+    {
+        Usage(argv[0]);
+        exit(1);
     }
 
     black_library::core::db::DBEntry create_staging_entry;
     black_library::core::db::DBEntry read_staging_entry;
     black_library::core::db::DBEntry update_staging_entry;
 
-
     std::cout << "Starting db" << std::endl;
-    black_library::core::db::BlackLibraryDB *blacklibrarydb = new black_library::core::db::BlackLibraryDB("/mnt/db/catalog.db", true);
+    black_library::core::db::BlackLibraryDB *blacklibrarydb = new black_library::core::db::BlackLibraryDB(opts.db_path, opts.intialize_db);
 
     create_staging_entry.UUID = "55ee59ad-2feb-4196-960b-3226c65c80d5";
     create_staging_entry.title = "foo2";
@@ -116,6 +175,8 @@ int main(int argc, char* argv[])
     std::cout << "\tuser_contributed: " << read_staging_entry.user_contributed << std::endl;
 
     std::cout << "Closing application" << std::endl;
+
+    delete blacklibrarydb;
 
     return 0;
 }
