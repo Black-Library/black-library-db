a429118616e593e9fabb426a955b0b59c60ce8af
Benjamin Chang <benjaminchang23@gmail.com>
2021-02-18 12:56:09 -0500

    update tag and comment of docker cmake file

--- cmake/docker.cmake
+++ cmake/docker.cmake
@@ -1,12 +1,12 @@
 macro(add_docker_targets)
     add_custom_target(bl-db-builder
-        COMMAND docker build --force-rm --tag bl/parsers/builder --file tools/docker/bl_db_builder.docker .
+        COMMAND docker build --force-rm --tag bl/db/builder --file tools/docker/bl_db_builder.docker .
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        COMMENT "Building bl-parsers-builder."
+        COMMENT "Building bl-db-builder."
         VERBATIM)
     add_custom_target(bl-db-builder-minimal
-        COMMAND docker build --force-rm --tag bl/parsers/builder/minimal --file tools/docker/bl_db_builder_minimal.docker .
+        COMMAND docker build --force-rm --tag bl/db/builder/minimal --file tools/docker/bl_db_builder_minimal.docker .
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        COMMENT "Building bl-parsers-builder-minimal."
+        COMMENT "Building bl-db-builder-minimal."
         VERBATIM)
 endmacro()
